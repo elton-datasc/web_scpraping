c80f9abd6c8286ccae6c044bee8fbf8b6b359048
elton-datasc <elton-guilherme@hotmail.com>
2024-03-26 22:04:47 -0300

    git_ignore e headers

--- hearders.py
+++ hearders.py
@@ -1,5 +1,9 @@
 import requests
 
+#categorias : 25 funko pop, 23 presentes criativos, 33 colecionáveis, camisetas 133, almofadas 171, canecas criativas 159,luminárias 161 ]
+categoria =  input('insira a categoria:')
+pg = input('insira a páginas:')
+
 headers = {
     'Referer': 'https://www.toyshow.com.br/loja/catalogo.php?loja=460977&categoria=25&pg=1',
     'Upgrade-Insecure-Requests': '1',
@@ -11,10 +15,9 @@ headers = {
 
 params = {
     'loja': '460977',
-    'categoria': '25',
-    'pg': '1',
-}
+    'categoria': categoria,
+    'pg': pg }
 
-response = requests.get('https://www.toyshow.com.br/loja/catalogo.php', params=params, headers=headers)
+response = requests.get('https://www.toyshow.com.br/loja/catalogo.php', params=params, headers=headers, timeout= 5)
 
-print(params)
+print(response)
